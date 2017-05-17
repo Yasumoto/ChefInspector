4734b045c831faaefad28abb78432d1fd289ed35
Joe Smith <yasumoto7@gmail.com>
2017-05-16 17:43:00 -0700

    Initial Commit

---
+++ ChefInspector/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ChefInspector
+//
+//  Created by Joseph Mehdi Smith on 5/16/17.
+//  Copyright © 2017 Joe Smith. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ChefInspector/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ChefInspector
+//
+//  Created by Joseph Mehdi Smith on 5/16/17.
+//  Copyright © 2017 Joe Smith. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
